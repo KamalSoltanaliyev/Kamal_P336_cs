72531d1c2324f027587bb93a683d027890d4d3a2
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-10-31 16:07:05 +0400

    Add files via upload

---
+++ P336 cs Task1/KamalP336project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    public static int f(int a)
+    {
+        return a * a;
+    }
+    public static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(f(a));
+    }
+}
