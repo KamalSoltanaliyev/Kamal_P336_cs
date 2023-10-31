622196a52b82c1858b023b7404cd8f5a408c0c0d
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-10-31 17:39:13 +0400

    Add files via upload

---
+++ P336_cs_Task3/P336_cs_Task3/P336_cs_Taks2/P336_cs_Taks2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    public static double CircleArea(double r)
+    {
+        double pi = 3;
+        return pi * r * r;
+    }
+    public static double RectangleArea(double a, double b)
+    {
+        return a + b;
+    }
+    public static double TringleInscribedInCircle(double a, double b, double c, double r)
+    {
+        double p = (a+b+c)/2;
+        return p*r;
+    }
+}
