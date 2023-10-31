f37baa067eac4196873b467137396f022f7d6b08
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-10-31 16:36:41 +0400

    Add files via upload

---
+++ P336_cs_Task3/P336_cs_Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    public static int f(int max)
+    {
+        int sum = 0;
+        for (int i = 0; i < max; i++)
+        {
+            if(sum + i < max)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    static void Main()
+    {
+        Console.WriteLine(f(11));
+    }
+}
